5f0cd41af8c4422af6799cb6aca33498ee1d1d64
Sahith Bhamidipati <bhsahith@yahoo.com>
2015-11-17 17:31:46 -0800

    Created triggerFlipper and added stow and extend functions. The values are DUMMY VALUES

---
+++ FtcRobotController/src/main/java/edu/art/_4345/resQ/subsystems/TriggerFlipper.java
@@ -0,0 +1,28 @@
+package edu.art._4345.resQ.subsystems;
+
+import com.qualcomm.robotcore.hardware.Servo;
+
+/**
+ * Created by member on 11/17/15.
+ */
+public class TriggerFlipper {
+
+    private Servo flipper;
+
+    public TriggerFlipper(Servo flipper){
+        this.flipper = flipper;
+
+        this.flipper.scaleRange(0, 1);
+    }
+
+    public void stowLeft() {
+        flipper.setPosition(0);
+    }
+
+    public void stowRight(){
+        flipper.setPosition(1);
+    }
+    public void extend(){
+        flipper.setPosition(0.5);
+    }
+}
