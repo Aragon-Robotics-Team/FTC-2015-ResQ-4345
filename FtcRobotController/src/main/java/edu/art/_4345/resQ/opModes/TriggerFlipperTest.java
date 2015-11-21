1a8b7e3672ee7f1bf39eb0ea02d1c0e691adaaf9
Sahith Bhamidipati <bhsahith@yahoo.com>
2015-11-21 12:15:17 -0800

    Added a new flipper object to account for the second servo on the other side of the robot.

--- FtcRobotController/src/main/java/edu/art/_4345/resQ/opModes/TriggerFlipperTest.java
+++ FtcRobotController/src/main/java/edu/art/_4345/resQ/opModes/TriggerFlipperTest.java
@@ -9,23 +9,35 @@ import edu.art._4345.resQ.subsystems.TriggerFlipper;
  */
 public class TriggerFlipperTest extends OpMode{
 
-    TriggerFlipper flipper;
+    TriggerFlipper flipperLeft;
+    TriggerFlipper flipperRight;
 
     @Override
     public void init() {
-        flipper = new TriggerFlipper(hardwareMap.servo.get("trigger_flipper"));
+        flipperLeft = new TriggerFlipper(hardwareMap.servo.get("trigger_flipper_left"));
+        flipperRight = new TriggerFlipper(hardwareMap.servo.get("trigger_flipper_right"));
     }
 
     @Override
     public void loop() {
         if (gamepad1.left_bumper) {
-            if (!flipper.isExtended()) {
-                flipper.extend();
+            if (!flipperLeft.isExtended()) {
+                flipperLeft.extend();
                 while(gamepad1.left_bumper);
             }
             else {
-                flipper.stow();
+                flipperLeft.stow();
                 while(gamepad1.left_bumper);
+            }
+        }
+        if (gamepad1.left_trigger > 0.5) {
+            if (!flipperRight.isExtended()) {
+                flipperRight.extend();
+                while(gamepad1.left_trigger > 0.5);
+            }
+            else {
+                flipperLeft.stow();
+                while(gamepad1.left_trigger > 0.5);
             }
         }
     }
