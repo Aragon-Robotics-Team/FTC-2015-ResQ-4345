900d23079fb576e9bdfa4d5ad230fa6c86c137a1
Miles P. Olson <mpolson64@gmail.com>
2015-11-21 10:44:31 -0800

    Added OpMode for drivecontrolled using only our most needed subsystems

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -30,6 +30,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpModeRegister;
 
 import edu.art._4345.resQ.opModes.DrivetrainTest;
 import edu.art._4345.resQ.opModes.TriggerFlipperTest;
+import edu.art._4345.resQ.opModes.DrivetrainFlipperOnly;
 
 /**
  * Register Op Modes.
@@ -42,5 +43,6 @@ public class FtcOpModeRegister implements OpModeRegister {
 
         manager.register("Drivetrain Test", DrivetrainTest.class);
         manager.register("Trigger Flipper Test", TriggerFlipperTest.class);
+        manager.register("Drivetrain and Trigger", .DrivetrainTest.class);
     }
 }
--- FtcRobotController/src/main/java/edu/art/_4345/resQ/opModes/DriveTriggerOnly.java
+++ FtcRobotController/src/main/java/edu/art/_4345/resQ/opModes/DriveTriggerOnly.java
@@ -1,23 +1,17 @@
-package edu.art._4345.resQ.opModes;
-
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-
-import edu.art._4345.resQ.subsystems.TriggerFlipper;
-
-/**
- * Created by member on 11/19/15.
- */
-public class TriggerFlipperTest extends OpMode{
-
-    TriggerFlipper flipper;
+public class DriveTriggerOnly extends OpMode {
+    
+    private Drivetrain drivetrain;
+    private TriggerFlipper triggerFlipper;
 
     @Override
     public void init() {
+        drivetrain = new Drivetrain(hardwareMap.dcMotor.get("left_drive"), hardwareMap.dcMotor.get("right_drive"));
         flipper = new TriggerFlipper(hardwareMap.servo.get("trigger_flipper"));
     }
 
     @Override
     public void loop() {
+            drivetrain.arcadeDrive(-1 * gamepad1.left_stick_y, gamepad1.right_stick_x);
         if (gamepad1.left_bumper) {
             if (!flipper.isExtended()) {
                 flipper.extend();
