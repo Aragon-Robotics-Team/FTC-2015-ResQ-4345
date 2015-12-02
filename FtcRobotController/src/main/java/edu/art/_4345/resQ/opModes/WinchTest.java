96d2c080a9e3a2714fd0dded4af9ca0d48a4ea08
SourMongoose <chrisxiongyang@yahoo.com>
2015-12-02 18:55:12 -0500

    Changed winch test to use two-motor winch; deleted winch-drive test

--- FtcRobotController/src/main/java/edu/art/_4345/resQ/opModes/WinchTest.java
+++ FtcRobotController/src/main/java/edu/art/_4345/resQ/opModes/WinchTest.java
@@ -7,36 +7,28 @@ import edu.art._4345.resQ.subsystems.Winch;
 /**
  * Teleop Op mode for use testing the winch subsystem.
  */
-public class WinchTest extends OpMode{
+public class WinchTest extends OpMode {
 
     private Winch winch;
 
-    private final double INCREMENT = 0.005;
+    private final double TAPE_POWER = 0.25;
 
     @Override
     public void init() {
-        winch = new Winch(hardwareMap.dcMotor.get("tape_motor"), hardwareMap.dcMotor.get("aim_motor"), hardwareMap.dcMotor.get("pulley_motor"));
+        winch = new Winch(hardwareMap.dcMotor.get("tape_motor"), hardwareMap.dcMotor.get("aim_motor"));
     }
 
     @Override
     public void loop() {
-        //retracting/extending tape measure & string independently
-        winch.tape(2 * (gamepad1.left_trigger - 0.5) * winch.tapePower);
-        winch.pulley(2 * (gamepad1.right_trigger - 0.5) * winch.pulleyPower);
-
-        //controlling tape motor speed
-        if(gamepad1.dpad_up)
-            winch.changeTapePower(INCREMENT);
-        else if(gamepad1.dpad_down)
-            winch.changeTapePower(-INCREMENT);
-
-        //controlling pulley motor speed
-        if(gamepad1.y)
-            winch.changePulleyPower(INCREMENT);
-        else if(gamepad1.a)
-            winch.changePulleyPower(-INCREMENT);
-
-        telemetry.addData("Tape power: ", winch.tapePower);
-        telemetry.addData("Pulley power: ", winch.pulleyPower);
+        //retracting/extending tape measure
+        winch.tape(gamepad1.left_stick_y * TAPE_POWER);
+        
+        //aiming tape measure
+        if (gamepad2.left_bumper)
+            winch.aim(1);
+        else if(gamepad2.left_trigger > 0.5)
+            winch.aim(-1);
+        else
+            winch.aim(0);
     }
 }
