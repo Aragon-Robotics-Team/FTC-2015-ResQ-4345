d431640cfa6ce8c66d843aff8de80998bd9d6035
Miles P. Olson <mpolson64@gmail.com>
2015-12-05 16:37:44 -0800

    Modified TeleOp to work with just what we have functioning right now

--- FtcRobotController/src/main/java/edu/art/_4345/resQ/opModes/TeleOp.java
+++ FtcRobotController/src/main/java/edu/art/_4345/resQ/opModes/TeleOp.java
@@ -10,7 +10,6 @@ import edu.art._4345.resQ.subsystems.*;
  */
 public class TeleOp extends OpMode {
 
-    private Winch winch;
     private Drivetrain drivetrain;
     private TriggerFlipper leftFlipper, rightFlipper;
     private Plow plow;
@@ -19,7 +18,6 @@ public class TeleOp extends OpMode {
 
     @Override
     public void init() {
-        winch = new Winch(hardwareMap.dcMotor.get("tape_motor"), hardwareMap.dcMotor.get("aim_motor"), hardwareMap.dcMotor.get("pulley_motor"));
         drivetrain = new Drivetrain(hardwareMap.dcMotor.get("left_drive"), hardwareMap.dcMotor.get("right_drive"));
         leftFlipper = new TriggerFlipper(hardwareMap.servo.get("trigger_flipper_left"), true);
         rightFlipper = new TriggerFlipper(hardwareMap.servo.get("trigger_flipper_right"), false);
@@ -50,33 +48,6 @@ public class TeleOp extends OpMode {
             plow.block();
         }
 
-        //retracting/extending tape measure & string independently
-        winch.tape(gamepad2.left_stick_y * winch.tapePower);
-        winch.pulley(gamepad2.right_stick_y * winch.pulleyPower);
 
-        //aiming tape measure
-        if (gamepad2.left_bumper)
-            winch.aim(1);
-        else if(gamepad2.left_trigger > 0.5)
-            winch.aim(-1);
-        else
-            winch.aim(0);
-
-        //controlling tape motor speed
-        if(gamepad2.dpad_up) {
-            winch.changeTapePower(INCREMENT);
-        }
-        else if(gamepad2.dpad_down) {
-            winch.changeTapePower(-INCREMENT);
-        }
-
-        //controlling pulley motor speed
-        if(gamepad2.y)
-            winch.changePulleyPower(INCREMENT);
-        else if(gamepad2.x)
-            winch.changePulleyPower(-INCREMENT);
-
-        telemetry.addData("Tape power: ", winch.tapePower);
-        telemetry.addData("Pulley power: ", winch.pulleyPower);
     }
 }
